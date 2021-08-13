02f5ad3a6f7ef1e50c0c1737ea21ade07b058d46
ManasviKatakam <84728147+ManasviKatakam@users.noreply.github.com>
2021-08-13 20:59:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas, backgroundImage;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+var database;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+
+  drawSprites();  
+}
